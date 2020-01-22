0a2636edef8d8f8ce109c7b51bb4e6b6de25db73
Abe Ybarra and Geoff Goetz <geoff@>
2020-01-21 16:16:54 -0800

    create files, retrieve 3 char words

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("#sentence").submit(function (event) {
+    event.preventDefault();
+
+
+    var sentence = $("input#sentence1").val();
+    console.log(sentence);
+
+
+    var words = sentence.split(" ");
+    console.log(words);
+
+    var over3 = [];
+    words.forEach(function (word) {
+      if (word.length >= 3) {
+        over3.push(word);
+      }
+    });
+    console.log(over3);
+  });
+});
